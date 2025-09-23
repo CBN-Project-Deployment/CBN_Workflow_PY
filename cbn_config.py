a180c98d6f6e49dbb01ecc923ee42ccfed5e7bf2
Mrityunjai-demo <mrityunjaikumar.dwivedy@nttdata.com>
2025-09-24 01:20:43 +0530

    Update cbn_config.py

--- cbn_config.py
+++ cbn_config.py
@@ -1,9 +1,8 @@
 # cbn_config.py
 INPUT_DIR = "input_files"
 OUTPUT_DIR = "output_files"
-
 CBN_PROJECT = "YourProjectName"
 USERNAME = "your_username"
-ROPC_CLIENT_ID = "client_id_here"
-URL_BASE = "https://cbn.example.com/api"
-AUTH_ROPC_URL = "https://cbn.example.com/auth"
+ROPC_CLIENT_ID = "your_client_id"
+AUTH_ROPC_URL = "https://your-auth-url.com"
+URL_BASE = "https://your-api-url.com"
