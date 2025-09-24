6ea6ba4066a07a76a3858faa384489e07446892b
CBN_WorkFlow_Deployment <mrityunjaikumar.dwivedy@nttdata.com>
2025-09-24 21:59:37 +0530

    Update cbn_config.py

--- cbn_config.py
+++ cbn_config.py
@@ -10,7 +10,7 @@ USERNAME = "Vikas.Mapari@nttdata.com"
 ROPC_CLIENT_ID = "3de400cd-0002-45fe-b163-1a5111944aec"
 AUTH_ROPC_URL = "https://dedalowcodingb2cpro.b2clogin.com/dedalowcodingb2cpro.onmicrosoft.com/B2C_1_ROPC-SignIn/oauth2/v2.0/token"   # Replace with actual URL
 URL_BASE = "https://coding.nttdata.com/api/v2"                     # Replace with actual URL
-CBN_PROJECT = "0195dbd2-71c5-7913-8469-110fca99b8e3"
+CBN_PROJECT = "01987e72-407e-7bdd-85b1-41d678445bf0"
 
 INPUT_DIR = "input_files"
 OUTPUT_DIR = "output_files"
