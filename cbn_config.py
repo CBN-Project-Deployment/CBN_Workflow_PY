e0985e9cfd5359a6fba46b5f76e95a4589c2af05
CBN_WorkFlow_Deployment <mrityunjaikumar.dwivedy@nttdata.com>
2025-09-24 20:18:11 +0530

    Update cbn_config.py

--- cbn_config.py
+++ cbn_config.py
@@ -1,8 +1,17 @@
 # cbn_config.py
+#INPUT_DIR = "input_files"
+#OUTPUT_DIR = "output_files"
+#CBN_PROJECT = "YourProjectName"
+#USERNAME = "your_username"
+#ROPC_CLIENT_ID = "your_client_id"
+#AUTH_ROPC_URL = "https://your-auth-url.com"
+#URL_BASE = "https://your-api-url.com"
+USERNAME = "Vikas.Mapari@nttdata.com"
+ROPC_CLIENT_ID = "3de400cd-0002-45fe-b163-1a5111944aec"
+AUTH_ROPC_URL = "https://dedalowcodingb2cpro.b2clogin.com/dedalowcodingb2cpro.onmicrosoft.com/B2C_1_ROPC-SignIn/oaut…"   # Replace with actual URL
+URL_BASE = "https://coding.nttdata.com/api/v2"                     # Replace with actual URL
+CBN_PROJECT = "0195dbd2-71c5-7913-8469-110fca99b8e3"
+
 INPUT_DIR = "input_files"
 OUTPUT_DIR = "output_files"
-CBN_PROJECT = "YourProjectName"
-USERNAME = "your_username"
-ROPC_CLIENT_ID = "your_client_id"
-AUTH_ROPC_URL = "https://your-auth-url.com"
-URL_BASE = "https://your-api-url.com"
+
