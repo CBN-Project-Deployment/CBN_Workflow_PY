1dba64eb66b9cc01b4435ec096e2b9ef575d5f99
CBN_WorkFlow_Deployment <mrityunjaikumar.dwivedy@nttdata.com>
2025-09-25 13:54:14 +0530

    Update cbn_config.py
    namespace name added

--- cbn_config.py
+++ cbn_config.py
@@ -11,6 +11,7 @@ ROPC_CLIENT_ID = "3de400cd-0002-45fe-b163-1a5111944aec"
 AUTH_ROPC_URL = "https://dedalowcodingb2cpro.b2clogin.com/dedalowcodingb2cpro.onmicrosoft.com/B2C_1_ROPC-SignIn/oauth2/v2.0/token"   # Replace with actual URL
 URL_BASE = "https://coding.nttdata.com/api/v2"                     # Replace with actual URL
 CBN_PROJECT = "DB-POC"
+namespace = "deutsche-bank-poc"
 
 INPUT_DIR = "input_files"
 OUTPUT_DIR = "output_files"
