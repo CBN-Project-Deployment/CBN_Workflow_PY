9c347f4c2b088104df89330276c8e7089cf4ee89
di-devops-poc <106592300+di-devops-poc@users.noreply.github.com>
2025-09-26 14:31:55 +0530

    Update cbn_config.py

--- cbn_config.py
+++ cbn_config.py
@@ -6,7 +6,8 @@
 #ROPC_CLIENT_ID = "your_client_id"
 #AUTH_ROPC_URL = "https://your-auth-url.com"
 #URL_BASE = "https://your-api-url.com"
-USERNAME = "Vikas.Mapari@nttdata.com"
+#USERNAME = "Vikas.Mapari@nttdata.com"
+USERNAME = "shrikant.pangudwale@nttdata.com"
 ROPC_CLIENT_ID = "3de400cd-0002-45fe-b163-1a5111944aec"
 AUTH_ROPC_URL = "https://dedalowcodingb2cpro.b2clogin.com/dedalowcodingb2cpro.onmicrosoft.com/B2C_1_ROPC-SignIn/oauth2/v2.0/token"   # Replace with actual URL
 URL_BASE = "https://coding.nttdata.com/api/v2"                     # Replace with actual URL
